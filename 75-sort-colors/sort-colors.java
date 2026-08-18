91b1398859335dfaa796ad33dcf3b98d9358f8fb
Amogh Deshpande <amoghadeshpande@gmail.com>
2026-08-18 17:40:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (9.98%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] count = new int[3];
+        for(int c : nums){
+            count[c]++;
+        }
+        int index=0;
+        for(int i=0;i<3;i++){
+            while(count[i]>0){
+                nums[index]=i;
+                index++;
+                count[i]--;
+            }
+        }
+
+    }
+}
