7de312ce775182647b62366e4fd8b9d813436452
Amogh Deshpande <amoghadeshpande@gmail.com>
2026-08-20 20:22:31 +0530

    Time: 877 ms (8.77%) | Memory: 60 MB (16.33%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,82 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // if(nums==null || nums.length<3) return new ArrayList<>();
-        // // Set<List<Integer>> res = new HashSet<>();
-        // List<List<Integer>> res = new ArrayList<>();
-        // Arrays.sort(nums);
-        // for(int i=0;i<nums.length-2;i++){
-        //     int left=i+1;
-        //     int right=nums.length-1;
-        //     while(left<right){
-        //         int sum=nums[i]+nums[left]+nums[right];
-        //         if(sum==0){
-        //             res.add(Arrays.asList(nums[i],nums[left],nums[right]));
-        //             left++;
-        //             right--;
-
-        //         }else if(sum<0) left++;
-        //         else right--;
-        //     }
-        // }
-        // return new ArrayList<>(res);
-
-     
-
-        List<List<Integer>> result = new ArrayList<>();
-
+        if(nums==null || nums.length<3) return new ArrayList<>();
+        Set<List<Integer>> res = new HashSet<>();
         Arrays.sort(nums);
-
-        for(int i = 0; i < nums.length - 2; i++) {
-
-            // Skip duplicate values for i
-            if(i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while(left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if(sum == 0) {
-
-                    // Create a triplet
-                    List<Integer> triplet = new ArrayList<>();
-
-                    triplet.add(nums[i]);
-                    triplet.add(nums[left]);
-                    triplet.add(nums[right]);
-
-                    // Add triplet to result
-                    result.add(triplet);
-
+        for(int i=0;i<nums.length-2;i++){
+            int left=i+1;
+            int right=nums.length-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(sum==0){
+                    res.add(Arrays.asList(nums[i],nums[left],nums[right]));
                     left++;
                     right--;
 
-                    // Skip duplicate left values
-                    while(left < right && nums[left] == nums[left - 1]) {
-                        left++;
-                    }
-
-                    // Skip duplicate right values
-                    while(left < right && nums[right] == nums[right + 1]) {
-                        right--;
-                    }
-
-                }
-                else if(sum < 0) {
-                    left++;
-                }
-                else {
-                    right--;
-                }
+                }else if(sum<0) left++;
+                else right--;
             }
         }
-
-        return result;
- 
+        return new ArrayList<>(res);
         
     }
 }
