d54ae672a31c82a4772e216ad7b3ce6bb0c03aa6
Amogh Deshpande <amoghadeshpande@gmail.com>
2026-08-24 23:05:23 +0530

    Time: 332 ms (59.63%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -2,10 +2,10 @@
 select id , count(*) as num
 from 
     (
-        select requester_id as id from RequestAccepted 
+        select requester_id as id from RequestAccepted
         union all
-        select accepter_id as id from RequestAccepted 
-    ) as ok
+        select accepter_id as id from RequestAccepted
+    ) as okmedium
 group by id
 order by num desc
 limit 1;
