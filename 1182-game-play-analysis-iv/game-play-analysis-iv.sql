f4ee758e92e9b47d00cc9a1ed14cf5069f6fbf04
Amogh Deshpande <amoghadeshpande@gmail.com>
2026-08-27 23:43:08 +0530

    Time: 701 ms (27.89%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select 
+round(count(distinct player_id)/(select count(distinct player_id) from Activity),2) as fraction
+from Activity
+where (player_id ,  date_sub(event_date , INTERVAL 1 DAY)) in
+(
+select player_id , min(event_date) as first from Activity 
+group by player_id
+)
