dd0b0cedd977b3dba5d3afc1cbb32e83d1e36ae1
Amogh Deshpande <amoghadeshpande@gmail.com>
2026-09-05 10:11:29 +0530

    Time: 5 ms (66.41%) | Memory: 126.9 MB (48.85%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
@@ -4,11 +4,13 @@ class Solution {
         int min=nums[nums.length-1];
         int[] p = new int[nums.length];
         int[] s = new int[nums.length];
+
         for(int i=0;i<nums.length;i++){
-            max = Math.max(max,nums[i]);
+            max=Math.max(max,nums[i]);
             p[i]=max;
-            min = Math.min(min,nums[nums.length-1-i]);
-            s[nums.length-1-i]=min;
+            min=Math.min(min,nums[nums.length-1-i]);
+            s[nums.length-i-1]=min;
+            
         }
         for(int i=0;i<nums.length;i++){
             if(p[i]-s[i]<=k) return i;
