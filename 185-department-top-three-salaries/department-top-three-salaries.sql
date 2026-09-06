50f5d2a7b38a6e573c53f511afcbabddf5f467a0
Amogh Deshpande <amoghadeshpande@gmail.com>
2026-09-06 21:29:02 +0530

    Time: 1346 ms (28.97%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,12 +1,12 @@
 # Write your MySQL query statement below
-select d.name as Department,
-       e.name as Employee,
-       e.salary as Salary
-from
+select 
+       d.name as Department ,
+       e.name as Employee ,
+       e.salary as Salary from
 Employee e inner join Department d
-on e.departmentId = d.id
+on e.departmentId=d.id
 where 3 > (
-    select count(distinct e2.salary)
+    select count(distinct e2.Salary)
     from Employee e2
     where e2.salary>e.salary and e.departmentId = e2.departmentId
 )
