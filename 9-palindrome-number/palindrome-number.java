d4b600d5cd5936071939d0c15edde4e6ca54e51d
Amogh Deshpande <amoghadeshpande@gmail.com>
2026-09-14 18:58:02 +0530

    Time: 5 ms (82.80%) | Memory: 46 MB (32.99%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int rev = x;
         int sum=0;
+        int rev = x;
         while(x>0){
-            sum=sum*10 + (x%10);
+            sum= sum*10 + x%10;
             x=x/10;
         }
-        return rev==sum;
-        
+
+        return rev == sum;
     }
 }
